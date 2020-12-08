327c89694779926315e9e91ed978260a172ef30b
symonk <jackofspaces@gmail.com>
2020-12-08 00:54:42 +0000

    fix isort; manage parent package imports a bit better

--- restpite/__init__.py
+++ restpite/__init__.py
@@ -1,4 +1,4 @@
-"""Top-level package for Restpite."""
+"""isort:skip_file"""
 
 __author__ = """Simon Kerr"""
 __email__ = "jackofspaces@gmail.com"
@@ -7,5 +7,18 @@ __version__ = "0.1.1"
 
 from .configuration.config import RestpiteConfig
 from .http.communication import Request
+from .http.communication import Response
+from .hooks import post_receive
+from .hooks import post_send
+from .hooks import pre_receive
+from .hooks import pre_send
 
-__all__ = ["Request", "RestpiteConfig"]
+__all__ = [
+    "RestpiteConfig",
+    "Request",
+    "Response",
+    "pre_send",
+    "post_send",
+    "pre_receive",
+    "post_receive",
+]
--- restpite/hooks/decorators.py
+++ restpite/hooks/decorators.py
@@ -1,14 +1,18 @@
-def pre_send():
+from restpite import Request
+from restpite import Response
+
+
+def pre_send(request: Request):
     pass
 
 
-def post_send():
+def post_send(request: Request):
     pass
 
 
-def pre_receive():
+def pre_receive(response: Response):
     pass
 
 
-def post_receive():
+def post_receive(response: Response):
     pass
