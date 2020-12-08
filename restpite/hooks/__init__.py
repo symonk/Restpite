e402f958108042f36f09ce82276306d527422f3c
symonk <jackofspaces@gmail.com>
2020-12-08 00:41:32 +0000

    padding up placeholder decorators for hook implementations

---
+++ restpite/hooks/__init__.py
@@ -0,0 +1,6 @@
+from .decorators import post_receive
+from .decorators import post_send
+from .decorators import pre_receive
+from .decorators import pre_send
+
+__all__ = ["pre_send", "post_send", "pre_receive", "post_receive"]
---
+++ restpite/hooks/decorators.py
@@ -0,0 +1,14 @@
+def pre_send():
+    pass
+
+
+def post_send():
+    pass
+
+
+def pre_receive():
+    pass
+
+
+def post_receive():
+    pass
