1190fc79830c8aab55a0938f934a826699335cf3
symonk <jackofspaces@gmail.com>
2020-12-13 15:28:24 +0000

    start building out a session #8

--- restpite/__init__.py
+++ restpite/__init__.py
@@ -6,9 +6,9 @@ __version__ = "0.1.1"
 
 
 from .configuration.config import RestpiteConfig
-from .http.communication import RestpiteSession
-from .http.communication import Request
-from .http.communication import Response
+from .http.session import Session
+from .http.request import Request
+from .http.response import Response
 from .hooks import post_receive
 from .hooks import post_send
 from .hooks import pre_receive
@@ -16,7 +16,7 @@ from .hooks import pre_send
 
 __all__ = [
     "RestpiteConfig",
-    "RestpiteSession",
+    "Session",
     "Request",
     "Response",
     "pre_send",
--- tests/unit/test_query_string_params.py
+++ tests/unit/test_query_string_params.py
@@ -1,18 +1,2 @@
-from assertpy import assert_that
-from requests import RequestException
-
-from restpite import Request
-
-
 def test_query_string_params() -> None:
-    example_params = {"one": "one", "two": "two", "three": "three"}
-    assert_that(
-        Request(
-            url="http://www.google.com",
-            query_params=example_params,
-            raise_on_failure=True,
-            retryable=(5, RequestException),
-        )
-        .get()
-        .status_code
-    ).is_equal_to(200)
+    ...
