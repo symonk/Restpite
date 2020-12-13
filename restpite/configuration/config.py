577e9f5a7ab0bdc426fb79183c454a852efcf43a
symonk <jackofspaces@gmail.com>
2020-12-13 16:03:32 +0000

    updating travis

--- restpite/configuration/config.py
+++ restpite/configuration/config.py
@@ -1,3 +1,10 @@
+from typing import Mapping
+from typing import Optional
+from typing import Tuple
+
+
 class RestpiteConfig:
-    def __init__(self) -> None:
-        ...
+    def __init__(
+        self, default_headers: Optional[Tuple[str, Mapping[str, str]]]
+    ) -> None:
+        self.default_headers = default_headers
