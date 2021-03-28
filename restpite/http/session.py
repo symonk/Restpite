23f11551205d4cd2a70d2bfa05962e051272d4c0
symonk <jackofspaces@gmail.com>
2021-03-28 00:18:54 +0000

    implement dunder enter/exit on RestpiteSession

--- restpite/http/session.py
+++ restpite/http/session.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import types
 import typing
 
 import requests
@@ -25,7 +26,7 @@ class RestpiteSession:
 
     Restpite Session uses a `requests.Session` under the hood for now.
 
-    :param additional_headers: A dictionary of headers to include in all session requests.  These are applied
+    :param headers: A dictionary of headers to include in all session requests.  These are applied
     on top of the requests default headers  which are:
 
         `User-Agent` = `restpite-{respite-version}`
@@ -37,44 +38,41 @@ class RestpiteSession:
     :param listeners: Optional implementations of the `RestpiteListener` Protocol
     :param connection_timeout: How long we will wait for your client to establish a remote connection, defaults to 31.00
     :param read_timeout: How long we will wait for the server to send a response, defaults to 31.00
-    :param query_strings: A mapping of strings to indicate query string parameters, appended to all request urls
-    :param defer_response_body: Defer the downloading response bodies until response.content is accessed.  Care is
+    :param params: A mapping of strings to indicate query string parameters, appended to all request urls
+    :param stream: Defer the downloading response bodies until response.content is accessed.  Care is
     advised here as sessions will not be re-allocated back to the pool until the response body has been downloaded.
-    :param ssl_verification: Verify SSL certificates, raises a SSLError otherwise, defaults to True.  Supports
+    :param verify: Verify SSL certificates, raises a SSLError otherwise, defaults to True.  Supports
     passing a file path to either a CA_BUNDLE or directory with a certificates of trusted CAs as well as boolean.
     As per requests standard, directories of certificates here must of been parsed with the `c_rehash` utility
     supplied by OpenSSL.
     :param maximum_redirects_limit: integer of how many redirects requests permits before raising a
     `TooManyRedirects` exception.
-    :param transport_adapters: List of additional transport adapters to mount on the HTTP session.  By default
+    :param adapters: List of additional transport adapters to mount on the HTTP session.  By default
     session ships with a simple `requests.adapter.HTTPAdapter` listening for all traffic on both http
     and https respectively.
     :param user_agent: The User Agent string added to subsequent request headers, if omitted restpite will
     replace the user-agent header with its own using `restpite-{restpite-version}`.
-    :param authentication: Callable for a custom authentication to pass to requests.  Subclassing
-    `requests.auth.Authbase`
-    is advisable here.  Restpite also provides some implementations out of the box
+    :param auth: Callable for a custom authentication to pass to requests.  Subclassing
+    `requests.auth.Authbase` is advisable here.  Restpite also provides some implementations out of the box.
     """
 
     def __init__(
         self,
-        additional_headers: typing.Optional[typing.Dict[str, str]] = None,
+        headers: typing.Optional[typing.Dict[str, str]] = None,
         listeners: typing.Optional[
             typing.List[listener_protocols.RestpiteListener]
         ] = None,
         connection_timeout: float = 31.00,
         read_timeout: float = 31.00,
-        query_strings: typing.Optional[
+        params: typing.Optional[
             typing.Union[bytes, typing.MutableMapping[str, str]]
         ] = None,
-        defer_response_body: bool = False,
-        ssl_verification: typing.Union[bool, str] = True,
+        stream: bool = False,
+        verify: typing.Union[bool, str] = True,
         maximum_redirects_limit: int = 30,
-        transport_adapters: typing.Optional[
-            typing.List[http_protocols.Mountable]
-        ] = None,
+        adapters: typing.Optional[typing.List[http_protocols.Mountable]] = None,
         user_agent: typing.Optional[str] = None,
-        authentication: typing.Optional[
+        auth: typing.Optional[
             typing.Union[
                 typing.Tuple[str, str],
                 AuthBase,
@@ -82,19 +80,19 @@ class RestpiteSession:
             ]
         ] = None,
     ) -> None:
-        self.headers = additional_headers or {}
+        self.headers = headers or {}
         if user_agent:
             self.headers["User-Agent"] = user_agent or f"restpite-{__version__}"
         self.listeners = [] if listeners is None else listeners.copy()
         self.listeners.insert(0, builtin_listeners.LoggingListener())
         self.connection_timeout = connection_timeout
         self.read_timeout = read_timeout
-        self.query_strings = query_strings or {}
-        self.defer_response_body = defer_response_body
-        self.ssl_verification = ssl_verification
-        self.maximum_redirects_limit = maximum_redirects_limit
-        self.transport_adapters = transport_adapters or []
-        self.authentication = authentication
+        self.query_strings = params or {}
+        self.defer_response_body = stream
+        self.verify = verify
+        self.max_redirects = maximum_redirects_limit
+        self.adapters = adapters or []
+        self.auth = auth
         self.session = self._prepare_session()
 
     def __getattr__(self, item: str) -> typing.Any:
@@ -115,19 +113,22 @@ class RestpiteSession:
         """
         session = requests.Session()
         session.stream = self.defer_response_body
-        session.verify = self.ssl_verification
-        session.max_redirects = self.maximum_redirects_limit
+        session.verify = self.verify
+        session.max_redirects = self.max_redirects
         session.params = self.query_strings
         session.headers.update(self.headers)
-        session.auth = self.authentication
+        session.auth = self.auth
         # TODO: Finish session delegation
         # TODO: Missing (proxies, hooks, cert, trust_env, cookies, adapters)
         return session
 
     def __enter__(self) -> RestpiteSession:
-        # TODO: How do we make this work with the wrapped session?
         return self
 
-    def __exit__(self) -> None:
-        # TODO: How do we make this work with the wrapped session?
-        ...
+    def __exit__(
+        self,
+        exc_type: typing.Optional[typing.Type[BaseException]] = None,
+        exc_value: typing.Optional[BaseException] = None,
+        traceback: typing.Optional[types.TracebackType] = None,
+    ) -> None:
+        self.session.close()
