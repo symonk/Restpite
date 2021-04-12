c6757de8ad4eac8eec033ef6fd611924a6e6a81f
symonk <jackofspaces@gmail.com>
2021-04-12 20:17:15 +0100

    remove api module; remove report from running in tox

--- restpite/__init__.py
+++ restpite/__init__.py
@@ -4,17 +4,10 @@ from .http.response import RestpiteResponse
 from .protocols.restpite_protocols import Notifyable
 from .http.session import RestpiteSession
 from .http.request import RestpiteRequest
-from .api import get
 
 import logging
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
-__all__ = [
-    "RestpiteSession",
-    "RestpiteRequest",
-    "RestpiteResponse",
-    "get",
-    "Notifyable",
-]
+__all__ = ["RestpiteSession", "RestpiteRequest", "RestpiteResponse", "Notifyable"]
